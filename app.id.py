0cafbe150b07a15a61968f11051ac40c2eeed628
Ben Longbottom-Smith <benlongbottomsmith@gmail.com>
2016-03-02 02:49:02 +0000

    Update app.id.py

--- app.id.py
+++ app.id.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""
+""
 Created on Fri Aug 14 12:24:33 2015
 
 @author: Ben Longbottom-Smith
 
 creates an 'app.id' file and write my 'app id' into it. 
 """
-app_id = str("2a85cc08c277438ebbaf4ba4d656558c")
+app_id = str("") #insert your personal and unique app.id inside the "" brackets
 
 
 with open("app.id", mode="wt", encoding="utf8") as d:
